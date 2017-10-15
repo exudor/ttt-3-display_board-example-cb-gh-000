517c01cca49b07b6111f49a13b2b74be9d4d9efd
Nick Williams <nwilliams11@my.spscc.edu>
2017-10-15 02:11:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts "  | #{board[1]} | #{board[2]} #{board[3]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ",]
+
+ display_board(board)
+ display_board(board)
+ display_board(board)
